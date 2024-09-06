56bd6e62784b6443fc8b1db2adc33c3f3861faa8
JuanMatos1 <74882386+JuanMatos1@users.noreply.github.com>
2024-09-05 21:35:19 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Para parsear JSON en las solicitudes
+
+// Configuración de la base de datos PostgreSQL
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// Rutas
+app.get('/', (req, res) => {
+  res.send('API funcionando correctamente');
+});
+
+app.get('/salary', (req, res) => {
+  res.send('Getting Salary');
+});
+
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
